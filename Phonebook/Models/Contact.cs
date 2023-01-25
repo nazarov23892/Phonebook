155560d10ddebef39a6d4717667e8855bb7e7b84
Mikhail Nazarov <nazarov23892@gmail.com>
2023-01-25 19:59:06 +0600

    + Contact Entity

---
+++ Phonebook/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phonebook.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public string Phonenumber { get; set; }
+
+    }
+}
