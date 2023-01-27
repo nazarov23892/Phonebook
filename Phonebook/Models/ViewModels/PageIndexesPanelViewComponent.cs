8a9f237ec9884eda28f94f29045ddea1c00c3c84
Mikhail Nazarov <nazarov23892@gmail.com>
2023-01-27 09:52:11 +0600

    ContactList: extracted PageIndexesPanel ViewComponent

---
+++ Phonebook/Models/ViewModels/PageIndexesPanelViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace Phonebook.Models.ViewModels
+{
+    public class PageIndexesPanelViewComponent: ViewComponent
+    {
+        public async Task<ViewViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
