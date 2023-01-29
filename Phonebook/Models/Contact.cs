65a0bcec64f70a955758aa135589dbe8879324a3
Mikhail Nazarov <nazarov23892@gmail.com>
2023-01-29 09:55:29 +0600

    Contact item: applying validation attributes

--- Phonebook/Models/Contact.cs
+++ Phonebook/Models/Contact.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Phonebook.Models
 {
@@ -9,12 +10,20 @@ namespace Phonebook.Models
     {
         public int ContactId { get; set; }
 
+        [StringLength(maximumLength:20)]
         public string Firstname { get; set; }
 
+        [StringLength(maximumLength: 20)]
         public string Lastname { get; set; }
 
+        [StringLength(maximumLength: 20)]
         public string Patronymic { get; set; }
 
+        [Required]
+        [StringLength(maximumLength: 11, MinimumLength = 6, 
+            ErrorMessage = "phonenumber length must be between 6 and 11 digits")]
+        [RegularExpression(pattern: "([0-9]+)", 
+            ErrorMessage = "phonenumber should have only digits")]
         public string Phonenumber { get; set; }
 
     }
