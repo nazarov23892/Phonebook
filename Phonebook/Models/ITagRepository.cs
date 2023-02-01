a9a68801b0e0fc825dc6ed0948c36cc5eb4c53b7
Mikhail Nazarov <nazarov23892@gmail.com>
2023-02-01 15:39:48 +0600

    added TagRepository

---
+++ Phonebook/Models/ITagRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phonebook.Models
+{
+    public interface ITagRepository
+    {
+        public IEnumerable<string> Tags { get; }
+        
+        public void AddTag(string tag);
+
+        public void DeleteTag(string tag);
+    }
+}
---
+++ Phonebook/Models/SqlTagRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phonebook.Models
+{
+    public class SqlTagRepository : ITagRepository
+    {
+        private List<string> tags = new List<string>
+        {
+            "tag#1", "tag#2", "tag#3"
+        };
+
+        public IEnumerable<string> Tags 
+        { 
+            get => tags; 
+        }
+
+        public void AddTag(string tag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTag(string tag)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
