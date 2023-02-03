8cf729f6eabd91a6b4e254944706b3e5a0c0eaf2
Mikhail Nazarov <nazarov23892@gmail.com>
2023-02-03 08:48:34 +0600

    refactor: dublicated logic extracted into base class

--- Phonebook/Models/DbTool.cs
+++ Phonebook/Models/DbTool.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace Phonebook.Models
@@ -9,6 +10,36 @@ namespace Phonebook.Models
     public class DbTool
     {
         public string ConnectionString { get; set; }
+
+        protected void ExecuteSelect(string sqlSelectCommand, 
+            Action<IDataRecord> itemRowReadedProc, 
+            IEnumerable<SqlParameter> sqlParameters = null)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString: ConnectionString))
+            {
+               using (SqlCommand sqlCommand = new SqlCommand(cmdText: sqlSelectCommand, connection: sqlConnection))
+                {
+                    sqlConnection.Open();
+
+                    foreach (var sqlParameter in sqlParameters ?? Enumerable.Empty<SqlParameter>())
+                    {
+                        sqlCommand.Parameters.Add(sqlParameter);
+                    }
+                    using (SqlDataReader sqlReader = sqlCommand.ExecuteReader())
+                    {
+                        if (sqlReader != null && sqlReader.HasRows)
+                        {
+                            while (sqlReader.Read())
+                            {
+                                itemRowReadedProc(sqlReader);
+                            }
+                        }
+                    }
+                    sqlConnection.Close();
+                }
+            }
+            return;
+        }
 
         protected object ExecuteSqlCommand(string sqlCommand, IEnumerable<SqlParameter> sqlParameters, CommandExecuteMode sqlExecuteMode)
         {
